32cfaaff7ac064eb82f018e3363c38e0f3e5c46f
Max Shaptala <maxshaptala@gmail.com>
2020-11-15 09:31:58 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Java. Урок 4
+
+1. Stream API
+2. Input Output
+3. Multi Thread
---
+++ src/main/java/org/itstep/App.java
@@ -0,0 +1,14 @@
+package org.itstep;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        // 1. Stream API
+
+        // 2. Input Output
+
+        // 3. Multi Thread
+    }
+}
